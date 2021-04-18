294f3ec9190bcfbdc2d2e50958e560671132ce38
povle <pasha@blinov.co>
2021-04-18 22:06:44 +0300

    change file names

--- widgets/blocks/input_rows/__init__.py
+++ widgets/blocks/input_rows/__init__.py
@@ -1,4 +1,4 @@
-from .int import IntInputRow
-from .bool import BoolInputRow
+from .int_row import IntInputRow
+from .bool_row import BoolInputRow
 
 INPUT_ROW_TYPES = {bool: BoolInputRow, int: IntInputRow}
