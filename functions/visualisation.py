f4d6f92bda17fce0f3a479e33613adcfb6d980ea
povle <pasha@blinov.co>
2021-04-18 18:32:02 +0300

    add table_df visualisation function

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -1,6 +1,14 @@
 import pandas
+from matplotlib.axes import Axes
 
 
-def plot_dataframe(data: pandas.DataFrame, axes) -> pandas.DataFrame:
+def plot_df(data: pandas.DataFrame, axes: Axes) -> pandas.DataFrame:
     data.plot(ax=axes)
+    return True
+
+
+def table_df(data: pandas.DataFrame, axes: Axes) -> pandas.DataFrame:
+    axes.axis('off')
+    axes.axis('tight')
+    axes.table(cellText=data.values, colLabels=data.columns, loc='center')
     return True
