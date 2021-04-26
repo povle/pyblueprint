55af8f40108ab6de8f90f25f24acde8f7df581b8
povle <pasha@blinov.co>
2021-04-26 19:29:41 +0300

    add sqlite and xlsx support

--- functions/input.py
+++ functions/input.py
@@ -1,6 +1,19 @@
+import sqlite3
 import pandas
 
 
 def input_csv(path: str) -> pandas.DataFrame:
     """*.csv"""
     return pandas.read_csv(path)
+
+
+def input_xlsx(path: str) -> pandas.DataFrame:
+    """*.xlsx"""
+    return pandas.read_excel(path)
+
+
+def input_sqlite(path: str, table_name: str) -> pandas.DataFrame:
+    """*.sqlite3 *.sqlite *.db"""
+    name = table_name or 'data'
+    with sqlite3.connect(path) as conn:
+        return pandas.read_sql(f'select * from {name}', conn)
--- functions/output.py
+++ functions/output.py
@@ -1,7 +1,21 @@
-import pandas
+import sqlite3
+from pandas import DataFrame
 
 
-def output_csv(data: pandas.DataFrame, path: str) -> pandas.DataFrame:
+def output_csv(data: DataFrame, path: str) -> DataFrame:
     """*.csv"""
     data.to_csv(path, index=False)
     return True
+
+
+def output_xlsx(data: DataFrame, path: str) -> DataFrame:
+    """*.xlsx"""
+    data.to_excel(path, index=False)
+    return True
+
+
+def output_sqlite(data: DataFrame, path: str, table_name: str) -> DataFrame:
+    """*.sqlite3 *.sqlite *.db"""
+    name = table_name or 'data'
+    with sqlite3.connect(path) as conn:
+        data.to_sql(name, conn, if_exists='replace')
