b08a3ed7faff7d9d35a4474db12aef8f35763bc6
povle <pasha@blinov.co>
2021-04-26 14:55:58 +0300

    change docstrings

--- functions/processing.py
+++ functions/processing.py
@@ -2,15 +2,15 @@ import pandas
 
 
 def transpose(data: pandas.DataFrame) -> pandas.DataFrame:
-    """Транспонирует DataFrame"""
+    """Транспонировать DataFrame"""
     return data.transpose()
 
 
 def scale(data: pandas.DataFrame, factor: float) -> pandas.DataFrame:
-    """Умножает каждый элемент DataFrame на factor"""
+    """Умножить каждый элемент DataFrame на factor"""
     return data * factor
 
 
 def pow(data: pandas.DataFrame, power: int) -> pandas.DataFrame:
-    """Возводит каждый элемент DataFrame в степень power"""
+    """Возвести каждый элемент DataFrame в степень power"""
     return data.applymap(lambda x: x**power)
--- functions/visualisation.py
+++ functions/visualisation.py
@@ -3,13 +3,13 @@ from matplotlib.axes import Axes
 
 
 def plot_df(data: pandas.DataFrame, axes: Axes) -> pandas.DataFrame:
-    """Строит график DataFrame"""
+    """Построить график DataFrame"""
     data.plot(ax=axes)
     return True
 
 
 def table_df(data: pandas.DataFrame, axes: Axes) -> pandas.DataFrame:
-    """Строит таблицу DataFrame"""
+    """Построить таблицу DataFrame"""
     axes.axis('off')
     axes.axis('tight')
     axes.table(cellText=data.values, colLabels=data.columns, loc='center')
